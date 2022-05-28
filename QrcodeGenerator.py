9efef5fbf419c13fd4f0d5155907d66841d6df47
Sreenath V <sreenathsnvaltpb@gmail.com>
2022-05-28 11:50:27 +0530

    Creating  a GUI window for the app

--- QrcodeGenerator.py
+++ QrcodeGenerator.py
@@ -1,3 +1,22 @@
 from tkinter import *
 import qrcode
 
+
+#GUI for the app
+
+root = Tk()
+
+# globlal variables
+url_var = StringVar()
+bg_color,tcolor = "#112b3c","#ffffff"
+
+
+root.geometry("400x500")
+root.resizable(width=False,height= False)
+root.title("QRcode Generator")
+root.config(background="#112b3c")
+
+
+
+
+root.mainloop()
